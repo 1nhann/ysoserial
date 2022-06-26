d93243eb6ad5e5e8abbfb8a598a731593f409af1
1nhann <inhanxx@gmail.com>
2022-06-26 18:13:03 +0800

    update

--- src/main/java/ysoserial/payloads/util/Reflections.java
+++ src/main/java/ysoserial/payloads/util/Reflections.java
@@ -1,11 +1,17 @@
 package ysoserial.payloads.util;
 
-import com.nqzero.permit.Permit;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
 import sun.reflect.ReflectionFactory;
 
-import java.lang.reflect.*;
+import com.nqzero.permit.Permit;
 
+@SuppressWarnings ( "restriction" )
 public class Reflections {
+
     public static void setAccessible(AccessibleObject member) {
         String versionStr = System.getProperty("java.version");
         int javaVersion = Integer.parseInt(versionStr.split("\\.")[0]);
@@ -20,85 +26,67 @@ public class Reflections {
             member.setAccessible(true);
         }
     }
-    public static Field getField (final Class<?> clazz, final String fieldName ) throws Exception {
-        try {
-            Field field = clazz.getDeclaredField(fieldName);
-            if ( field != null )
-                field.setAccessible(true);
-            else if ( clazz.getSuperclass() != null )
-                field = getField(clazz.getSuperclass(), fieldName);
 
-            return field;
+    public static Field getField(final Class<?> clazz, final String fieldName) {
+        Field field = null;
+        try {
+            field = clazz.getDeclaredField(fieldName);
+            setAccessible(field);
         }
-        catch ( NoSuchFieldException e ) {
-            if ( !clazz.getSuperclass().equals(Object.class) ) {
-                return getField(clazz.getSuperclass(), fieldName);
-            }
-            throw e;
+        catch (NoSuchFieldException ex) {
+            if (clazz.getSuperclass() != null)
+                field = getField(clazz.getSuperclass(), fieldName);
         }
+        return field;
     }
 
-    public static void setFieldValue ( final Object obj, final String fieldName, final Object value ) throws Exception {
-        final Field field = getField(obj.getClass(), fieldName);
+    //	public static void setFieldValue(final Object obj, final String fieldName, final Object value) throws Exception {
+//		final Field field = getField(obj.getClass(), fieldName);
+//		field.set(obj, value);
+//	}
+    public static void setFieldValue(Object obj, String fieldName, Object value) throws Exception {
+        Field field = getField(obj.getClass(), fieldName);
         Field modifersField = Field.class.getDeclaredField("modifiers");
         modifersField.setAccessible(true);
-        modifersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+        modifersField.setInt(field, field.getModifiers() & -17);
         field.set(obj, value);
     }
-    public static void setStaticFieldValue (final Class clazz, final String fieldName, final Object value ) throws Exception {
-        final Field field = getField(clazz, fieldName);
+    public static void setStaticFieldValue(Class clazz, String fieldName, Object value) throws Exception {
+        Field field = getField(clazz, fieldName);
         Field modifersField = Field.class.getDeclaredField("modifiers");
         modifersField.setAccessible(true);
-        modifersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-        field.set(null, value);
+        modifersField.setInt(field, field.getModifiers() & -17);
+        field.set((Object)null, value);
     }
-//    public static void setFinalFieldValue ( final Object obj, final String fieldName, final Object value ) throws Exception {
-//        Field f = obj.getClass().getDeclaredField(fieldName);
-//        f.setAccessible(true);
-//        Field modifersField = Field.class.getDeclaredField("modifiers");
-//        modifersField.setAccessible(true);
-//        modifersField.setInt(f, f.getModifiers() & ~Modifier.FINAL);
-//        f.set(obj,value);
-//    }
-//    public static void setStaticFinalFieldValue (final Class clazz, final String fieldName, final Object value ) throws Exception {
-//        Field f = clazz.getDeclaredField(fieldName);
-//        f.setAccessible(true);
-//        Field modifersField = Field.class.getDeclaredField("modifiers");
-//        modifersField.setAccessible(true);
-//        modifersField.setInt(f, f.getModifiers() & ~Modifier.FINAL);
-//        f.set(null,value);
-//    }
 
-
-    public static Object getFieldValue ( final Object obj, final String fieldName ) throws Exception {
+    public static Object getFieldValue(final Object obj, final String fieldName) throws Exception {
         final Field field = getField(obj.getClass(), fieldName);
         return field.get(obj);
     }
 
-
-    public static Constructor<?> getFirstCtor (final String name ) throws Exception {
-        final Constructor<?> ctor = Class.forName(name).getDeclaredConstructors()[ 0 ];
-        ctor.setAccessible(true);
+    public static Constructor<?> getFirstCtor(final String name) throws Exception {
+        final Constructor<?> ctor = Class.forName(name).getDeclaredConstructors()[0];
+        setAccessible(ctor);
         return ctor;
     }
 
+    public static Object newInstance(String className, Object ... args) throws Exception {
+        return getFirstCtor(className).newInstance(args);
+    }
 
     public static <T> T createWithoutConstructor ( Class<T> classToInstantiate )
-            throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         return createWithConstructor(classToInstantiate, Object.class, new Class[0], new Object[0]);
     }
 
-
-    @SuppressWarnings ( {
-            "unchecked"
-    } )
-    public static <T> T createWithConstructor ( Class<T> classToInstantiate, Class<? super T> constructorClass, Class<?>[] consArgTypes,
-                                                Object[] consArgs ) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+    @SuppressWarnings ( {"unchecked"} )
+    public static <T> T createWithConstructor ( Class<T> classToInstantiate, Class<? super T> constructorClass, Class<?>[] consArgTypes, Object[] consArgs )
+        throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         Constructor<? super T> objCons = constructorClass.getDeclaredConstructor(consArgTypes);
-        objCons.setAccessible(true);
+        setAccessible(objCons);
         Constructor<?> sc = ReflectionFactory.getReflectionFactory().newConstructorForSerialization(classToInstantiate, objCons);
-        sc.setAccessible(true);
-        return (T) sc.newInstance(consArgs);
+        setAccessible(sc);
+        return (T)sc.newInstance(consArgs);
     }
 
 }
